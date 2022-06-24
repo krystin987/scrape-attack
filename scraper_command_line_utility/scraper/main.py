fbf13960ec91cf0133ab4aa37ef739e4b8210d33
Matt Hayden <hayden+gitlab@morp.holo.gy>
2022-06-23 22:32:56 -0600

    Tidy up

--- scraper_command_line_utility/scraper/main.py
+++ scraper_command_line_utility/scraper/main.py
@@ -2,6 +2,9 @@
 import sys
 
 def main():  # pylint: disable=inconsistent-return-statements
+    """
+    This just prints "Wicked cool arguments: ..." for the purposes of testing
+    """
     # pylint: disable=broad-except
     args = sys.argv[1:]  # By UNIX convention, sys.argv is a list of arguments, except the first one is the filename of this script
     print("Wicked cool arguments:", args)
