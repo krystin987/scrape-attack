9afb13f4b3095f60358c8041d2b314ee96ed1004
Krystin Villeneuve <krystinv@gmail.com>
2022-11-03 12:18:07 -0600

    adjust directory structure for static lists

--- scraper_command_line_utility/scraper/paths.py
+++ scraper_command_line_utility/scraper/paths.py
@@ -6,30 +6,27 @@ import json
 from pathlib import Path
 import os
 
-CACHE_DIR = Path.home() / ".cache" / "story_info"
-CONFIG_DIR = Path.home() / ".config" / "story_info"
-CACHED_STATIONINDEX = CACHE_DIR / "all-stations.json"
+
+ROOT_DIR = Path.cwd()
+CACHE_DIR = ROOT_DIR / 'scraper/cache/story_info/'
+CONFIG_DIR = ROOT_DIR / 'scraper/config/story_info/'
+# CACHED_STATIONINDEX = CACHE_DIR / "all-stations.json"
 
 def get_cache_path(name):
-    if CACHE_DIR.exists():
-        print("Skipping")
-    else:
-        CACHE_DIR.mkdir(parents=True)
     parent = CACHE_DIR / "incoming_web_data_zips"
     os.makedirs(name, exist_ok=True)
     return parent / f"{name}_{datetime.now():%Y_%m_%d_%p}.zip"
 
 def load_rss_config():
-    if CONFIG_DIR.exists():
-        print("Skipping")
-    else:
-        CONFIG_DIR.mkdir(parents=True)
     return json.load((CONFIG_DIR / "rss_feeds.json").open())
 
 def load_whitelist():
-    
     return (CONFIG_DIR / "whitelist.txt").read_text().splitlines()
 
 def load_nopelist():
     return (CONFIG_DIR / "nopelist.txt").read_text().splitlines()
 
+
+# old paths, might reimplement
+# CACHE_DIR = Path.home() / ".cache" / "story"
+# CONFIG_DIR = Path.home() / ".config" / "story"
